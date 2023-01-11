6ad735b362293064a8c9e0bed3c3d4f430b8e80a
Alex <codeetc@pm.me>
2023-01-11 23:44:14 +0000

    refactor: partial pagination support

--- src/api.rs
+++ src/api.rs
@@ -1,119 +1,142 @@
-use self::rest::{
-    models::{Exchange, Market, MarketSummary},
-    RESTResponse,
-};
+use self::rest::{ExchangeAPI, MarketAPI};
 
 pub mod markets;
 
 pub mod rest {
-    use self::models::Allowance;
+    use self::models::{Allowance, Cursor, Exchange, Market, MarketSummary};
 
     #[derive(serde::Deserialize)]
     #[serde()]
     pub struct RESTResponse {
-        pub error: Option<String>,
         pub allowance: Allowance,
+        pub cursor: Option<Cursor>,
+        pub error: Option<String>,
         pub result: Option<serde_json::Value>,
     }
 
     pub mod models {
         include!(concat!(env!("OUT_DIR"), "/cryptowatch.rest.models.rs"));
     }
-}
 
-pub struct MarketAPI {
-    base_url: &'static str,
-}
+    pub struct MarketAPI {
+        base_url: &'static str,
+    }
 
-impl MarketAPI {
-    pub async fn list(&self) -> Result<Vec<Market>, String> {
-        let url = format!("{}/markets", self.base_url);
-        let resp: RESTResponse = reqwest::get(url)
-            .await
-            .expect("Failed to get markets")
-            .json()
-            .await
-            .expect("Failed to serialise markets response to JSON");
-        if let Some(markets) = resp.result {
-            Ok(serde_json::from_value(markets).expect("Not a markets response"))
-        } else if let Some(error) = resp.error {
-            Err(error.clone())
-        } else {
-            Err("No normal or error response available".into())
+    #[derive(Debug)]
+    pub struct MarketPage {
+        pub cursor: Option<Cursor>,
+        pub markets: Vec<Market>,
+    }
+
+    impl MarketAPI {
+        pub(crate) fn new(base_url: &'static str) -> Self {
+            Self { base_url }
+        }
+
+        pub async fn list(&self, cursor: Option<Cursor>) -> Result<MarketPage, String> {
+            let url = if let Some(c) = cursor {
+                if c.has_more {
+                    format!("{}/markets?cursor={}", self.base_url, c.last)
+                } else {
+                    format!("{}/markets", self.base_url)
+                }
+            } else {
+                format!("{}/markets", self.base_url)
+            };
+            let resp: RESTResponse = reqwest::get(url)
+                .await
+                .expect("Failed to get markets")
+                .json()
+                .await
+                .expect("Failed to serialise markets response to JSON");
+            if let Some(markets) = resp.result {
+                Ok(MarketPage {
+                    cursor: resp.cursor,
+                    markets: serde_json::from_value(markets).expect("Not a markets response"),
+                })
+            } else if let Some(error) = resp.error {
+                Err(error.clone())
+            } else {
+                Err("No normal or error response available".into())
+            }
+        }
+
+        pub async fn summary(&self, exchange: &str, pair: &str) -> Result<MarketSummary, String> {
+            let url = format!("{}/markets/{}/{}/summary", self.base_url, exchange, pair);
+            let resp: RESTResponse = reqwest::get(url)
+                .await
+                .expect("Failed to get market summary")
+                .json()
+                .await
+                .expect("Failed to serialise market summary response to JSON");
+            if let Some(summary) = resp.result {
+                Ok(serde_json::from_value(summary).expect("Not a market summary response"))
+            } else if let Some(error) = resp.error {
+                Err(error.clone())
+            } else {
+                Err("No normal or error response available".into())
+            }
         }
     }
 
-    pub async fn summary(&self, exchange: &str, pair: &str) -> Result<MarketSummary, String> {
-        let url = format!("{}/markets/{}/{}/summary", self.base_url, exchange, pair);
-        let resp: RESTResponse = reqwest::get(url)
-            .await
-            .expect("Failed to get market summary")
-            .json()
-            .await
-            .expect("Failed to serialise market summary response to JSON");
-        if let Some(summary) = resp.result {
-            Ok(serde_json::from_value(summary).expect("Not a market summary response"))
-        } else if let Some(error) = resp.error {
-            Err(error.clone())
-        } else {
-            Err("No normal or error response available".into())
-        }
-    }
-}
-
-pub struct ExchangeAPI {
-    base_url: &'static str,
-}
-
-impl ExchangeAPI {
-    pub async fn list(&self) -> Result<Vec<Exchange>, String> {
-        let url = format!("{}/exchanges", self.base_url);
-        let resp: RESTResponse = reqwest::get(url)
-            .await
-            .expect("Failed to get exchanges")
-            .json()
-            .await
-            .expect("");
-        if let Some(exchanges) = resp.result {
-            Ok(serde_json::from_value(exchanges).expect("Not an exchanges response"))
-        } else if let Some(ref error) = resp.error {
-            Err(error.clone())
-        } else {
-            Err("No normal or error response available".into())
-        }
+    pub struct ExchangeAPI {
+        base_url: &'static str,
     }
 
-    pub async fn detail(&self, name: &str) -> Result<Exchange, String> {
-        let url = format!("{}/exchanges/{}", self.base_url, name);
-        let resp: RESTResponse = reqwest::get(url)
-            .await
-            .expect("Failed to get exchange detail")
-            .json()
-            .await
-            .expect("Couldn't serialise exchange response to JSON");
-        if let Some(exchange) = resp.result {
-            Ok(serde_json::from_value(exchange).expect("Not an exchange response"))
-        } else if let Some(ref error) = resp.error {
-            Err(error.clone())
-        } else {
-            Err("No normal or error response available".into())
+    impl ExchangeAPI {
+        pub(crate) fn new(base_url: &'static str) -> Self {
+            Self { base_url }
         }
-    }
 
-    pub async fn markets(&self, name: &str) -> Result<Vec<Market>, String> {
-        let url = format!("{}/markets/{}", self.base_url, name);
-        let resp: RESTResponse = reqwest::get(url)
-            .await
-            .expect("Failed to get markets")
-            .json()
-            .await
-            .expect("Failed to serialise markets response to JSON");
-        if let Some(markets) = resp.result {
-            Ok(serde_json::from_value(markets).expect("Not a markets response"))
-        } else if let Some(error) = resp.error {
-            Err(error.clone())
-        } else {
-            Err("No normal or error response available".into())
+        pub async fn list(&self) -> Result<Vec<Exchange>, String> {
+            let url = format!("{}/exchanges", self.base_url);
+            let resp: RESTResponse = reqwest::get(url)
+                .await
+                .expect("Failed to get exchanges")
+                .json()
+                .await
+                .expect("");
+            if let Some(exchanges) = resp.result {
+                Ok(serde_json::from_value(exchanges).expect("Not an exchanges response"))
+            } else if let Some(ref error) = resp.error {
+                Err(error.clone())
+            } else {
+                Err("No normal or error response available".into())
+            }
+        }
+
+        pub async fn detail(&self, name: &str) -> Result<Exchange, String> {
+            let url = format!("{}/exchanges/{}", self.base_url, name);
+            let resp: RESTResponse = reqwest::get(url)
+                .await
+                .expect("Failed to get exchange detail")
+                .json()
+                .await
+                .expect("Couldn't serialise exchange response to JSON");
+            if let Some(exchange) = resp.result {
+                Ok(serde_json::from_value(exchange).expect("Not an exchange response"))
+            } else if let Some(ref error) = resp.error {
+                Err(error.clone())
+            } else {
+                Err("No normal or error response available".into())
+            }
+        }
+
+        pub async fn markets(&self, name: &str) -> Result<Vec<Market>, String> {
+            let url = format!("{}/markets/{}", self.base_url, name);
+            let resp: RESTResponse = reqwest::get(url)
+                .await
+                .expect("Failed to get markets")
+                .json()
+                .await
+                .expect("Failed to serialise markets response to JSON");
+            if let Some(markets) = resp.result {
+                Ok(serde_json::from_value(markets).expect("Not a markets response"))
+            } else if let Some(error) = resp.error {
+                Err(error.clone())
+            } else {
+                Err("No normal or error response available".into())
+            }
         }
     }
 }
@@ -159,14 +182,10 @@ impl Default for Cryptowatch {
 #[async_trait::async_trait]
 impl CryptowatchAPI for Cryptowatch {
     fn market(&self) -> MarketAPI {
-        MarketAPI {
-            base_url: self.base_url.clone(),
-        }
+        MarketAPI::new(self.base_url.clone())
     }
 
     fn exchange(&self) -> ExchangeAPI {
-        ExchangeAPI {
-            base_url: self.base_url.clone(),
-        }
+        ExchangeAPI::new(self.base_url.clone())
     }
 }
--- tests/markets_itest.rs
+++ tests/markets_itest.rs
@@ -1,16 +1,23 @@
-use cryptowatch::api::rest::models::{Market, MarketSummary};
+use cryptowatch::api::rest::models::MarketSummary;
+use cryptowatch::api::rest::MarketPage;
 use cryptowatch::api::{Cryptowatch, CryptowatchAPI};
 
 #[tokio::test]
 async fn test_markets() {
     let api = Cryptowatch::default();
-    let markets: Vec<Market> = api.market().list().await.unwrap();
-    assert_ne!(0, markets.len());
-    let pairs: Vec<String> = markets.into_iter().map(|m| m.pair).collect();
+    let page1: MarketPage = api.market().list(None).await.unwrap();
+    assert_ne!(0, page1.markets.len());
+    assert!(page1.cursor.as_ref().map(|c| c.has_more).unwrap_or(false));
+    let pairs1: Vec<String> = page1.markets.into_iter().map(|m| m.pair).collect();
 
     for pair in vec!["btcgbp", "btceur", "btcusd"].into_iter() {
-        assert!(pairs.contains(&pair.to_owned()), "Missing {}", pair);
+        assert!(pairs1.contains(&pair.to_owned()), "Missing {}", pair);
     }
+
+    let page2: MarketPage = api.market().list(page1.cursor).await.unwrap();
+    assert_ne!(0, page2.markets.len());
+    assert_eq!(page2.cursor.as_ref().map(|c| c.has_more).unwrap_or(false), false);
+
 }
 
 #[tokio::test]
@@ -27,5 +34,4 @@ async fn test_market_summary() {
     assert!(delta.percentage > 0.);
     assert!(summary.volume > 0.);
     assert!(summary.volume_quote > 0.);
-
 }
